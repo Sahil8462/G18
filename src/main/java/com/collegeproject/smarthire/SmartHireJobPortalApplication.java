12dacedeb40755e19ac815ccdbffbd984e4361aa
Sahil <sahilkum097@gmail.com>
2025-09-23 20:03:15 +0530

    Initial commit

---
+++ src/main/java/com/collegeproject/smarthire/SmartHireJobPortalApplication.java
@@ -0,0 +1,13 @@
+package com.collegeproject.smarthire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHireJobPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHireJobPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collegeproject/smarthire/SmartHireJobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegeproject.smarthire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHireJobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
